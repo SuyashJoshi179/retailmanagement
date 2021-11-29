4edfe6a67fe1ad9570e171aad272c760658eabcd
SuyashJoshi179 <suyash.joshi179@gmail.com>
2021-11-29 09:08:28 +0530

    Login Page Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {
   BrowserRouter as Router,
   Routes, Route
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Customer from './components/CustomerDashboard';
 import Delivery from './components/DeliveryPersonelDash';
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,44 @@
-
-const Home = (props) => {
-    return(<h1>Hi</h1>);
+import { Button, Card, CardBody, CardText, Form, FormGroup, Input, Label } from "reactstrap";
+const Customer = (props) => {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <Card className='cardform'>
+                    <CardBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="Username">
+                                    <strong>Enter Username</strong>
+                                </Label>
+                                <Input
+                                    id="username"
+                                    name="username"
+                                    placeholder="Username"
+                                    type="text"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="Password">
+                                    <strong>Enter Password</strong>
+                                </Label>
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    type="password"
+                                />
+                            </FormGroup>
+                            <Button className="formbutton">
+                                Submit
+                            </Button>
+                            <CardText className="signintext">
+                                Don't have an account? <strong><a href='/'>Sign Up.</a></strong>
+                            </CardText>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
+    );
 }
-export default Home;
+export default Customer;
